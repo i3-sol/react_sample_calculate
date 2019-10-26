448faa737a1522655cc0ffac2bdf2c45a9a94ffc
ThunderBird0121 <spacecalian0121@gmail.com>
2019-10-26 13:34:36 +0530

    Added proptypes and a very basic test in Info file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Info from './Info';
 import Bar from './Bar';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
+const uuidv1 = require('uuid/v4');
 
 const App = () => {
   let initialState = () => JSON.parse(localStorage.getItem('data')) || [];
@@ -13,6 +14,7 @@ const App = () => {
   const handleChange = val => {
     let heightInM = val.height / 100;
     val.bmi = (val.weight / (heightInM * heightInM)).toFixed(2);
+    val.id = uuidv1();
     let newVal = [...state, val];
     let len = newVal.length;
     if (len > 7) newVal = newVal.slice(1, len);
@@ -20,15 +22,15 @@ const App = () => {
   };
 
   const handleDelete = id => {
-    localStorage.setItem('lastState', JSON.stringify(state))
+    localStorage.setItem('lastState', JSON.stringify(state));
     let newState = state.filter(i => {
       return i.id !== id;
     });
     setState(newState);
   };
-  const handleUndo = function() {
-      setState(JSON.parse(localStorage.getItem('lastState')));
-  }
+  const handleUndo = () => {
+    setState(JSON.parse(localStorage.getItem('lastState')));
+  };
   useEffect(() => {
     localStorage.setItem('data', JSON.stringify(state));
     const date = state.map(obj => obj.date);
@@ -46,40 +48,39 @@ const App = () => {
         <div className="col m12 s12">
           <BmiForm change={handleChange} />
           <Bar labelData={data.date} bmiData={data.bmi} />
-          {state.length > 0 ? (
-            <div>
-              <div className="row center">
-                <h4 className="white-text">7 Day Data</h4>
-              </div>
-              <div className="data-container row">
-                {state.map(info => (
-                  <Info
-                    key={info.id}
-                    id={info.id}
-                    weight={info.weight}
-                    height={info.height}
-                    date={info.date}
-                    bmi={info.bmi}
-                    deleteCard={handleDelete}
-                  />
-                ))}
-              </div>
+          <div>
+            <div className="row center">
+              <h4 className="white-text">7 Day Data</h4>
+            </div>
+            <div className="data-container row">
+              {state.length > 0 ? (
+                <>
+                  {state.map(info => (
+                    <Info
+                      key={info.id}
+                      id={info.id}
+                      weight={info.weight}
+                      height={info.height}
+                      date={info.date}
+                      bmi={info.bmi}
+                      deleteCard={handleDelete}
+                    />
+                  ))}
+                </>
+              ) : (
+                <div className="center white-text">No log found</div>
+              )}
+            </div>
+          </div>
+          {localStorage.getItem('lastState') !== null ? (
+            <div className="center">
+              <button className="calculate-btn" onClick={handleUndo}>
+                Undo
+              </button>
             </div>
           ) : (
-            <div>
-              <div className="row center">
-                <h4 className="white-text">7 Day Data</h4>
-              </div>
-              <div className="data-container row">
-              <div className="center white-text">No log found</div>
-              </div>
-            </div>
+            ''
           )}
-          {localStorage.getItem('lastState') !== null ? (
-                    <div className="center">
-                        <button className="calculate-btn" onClick={handleUndo}>Undo</button>
-                    </div>
-                ) : ('')}
         </div>
       </div>
     </div>
